163dc8766f06c4adfce9a7b367b0d2ac9db31792
Akansha <akansha.ak048@gmail.com>
2026-06-28 23:04:13 +0530

    fixing code

--- backend/server.js
+++ backend/server.js
@@ -12,28 +12,7 @@ app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
-const taskRoutes = require('./routes/tasks');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/tasks', taskRoutes);
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => console.log('Connection error:', err));
+mongoose.connect(process.env.MONGO_URI, { dbName: 'tasktracker' })
   .then(() => {
     console.log('MongoDB connected');
     app.listen(process.env.PORT, () => {
